d2d4c7a0f8695decbb69ccb60515a0a7c3807400
Gargantuan5k <siddharth.vivek2000@outlook.com>
2021-04-03 14:17:40 +0530

    MELONS :D

--- prog1.cpp
+++ prog1.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
 int main() {
-    system("clear");
-    std::cout << "Not Not Hello World :)";
+    system("clear");    
+    
+    char likeMelons;
+    int slices;
+
+    std::cout << "Oi dyou like melons? "; std::cin >> likeMelons;
+
+    if (likeMelons == 'y') {
+        std::cout << "YOO Epic how many melon slices dyou eat in a day? "; std::cin >> slices;
+
+        if (slices >= 69420) {
+            std::cout << "ALL HAIL THE MELON KING YAYAYA";
+        } else {
+            std::cout << "Lmao normie only eats " << slices << " slices of melon in a day. Shame.";
+        }
+
+    } else {
+        std::cout << "Kil";
+    }
+
     return 0;
 }
+    
